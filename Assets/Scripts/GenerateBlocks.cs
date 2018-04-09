4daa9bfcf144b6e1a3d930d78e157b4d3009c9ca
Luke <916103598+Luke@users.noreply.github.com>
2018-04-09 22:22:57 +0800

    GenerateTest-2018/4/9
    Date:2018/4/9
    Content:
        1.Create prefab - block
        2.Generate 100 * 100 blocks to test

---
+++ Assets/Scripts/GenerateBlocks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBlocks : MonoBehaviour {
+
+    public GameObject Block; //the block will be generated
+    private int m_generateWidth = 100; //the width of terrian made of blocks
+
+    // Use this for initialization
+    void Start() {
+        //generate 100 * 100 blocks to test
+        GameObject blockSets = new GameObject("BlockSets");
+        for (int row = 0; row < m_generateWidth; row++) {
+            for (int cloum = 0; cloum < m_generateWidth; cloum++) {
+                GameObject block = GameObject.Instantiate(Block, blockSets.transform);
+                block.transform.position = new Vector3(-m_generateWidth + row, 0, -m_generateWidth + cloum);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
